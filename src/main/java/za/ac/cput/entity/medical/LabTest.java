ada033638055a58cae08448ef8e05d0fdbd868c6
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-08-09 14:51:43 +0200

    Updated the labTest class Entity

--- src/main/java/za/ac/cput/entity/medical/LabTest.java
+++ src/main/java/za/ac/cput/entity/medical/LabTest.java
@@ -2,96 +2,48 @@ package za.ac.cput.entity.medical;
 /*
 LabTest.java
 Author:Kevin Lionel Mombo Ndinga(218180500)
-Date: 07 April 2022
+Date: 09 August 2022;
 */
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "lab_test")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
 
 public class LabTest {
+    @Id
+    @Column(name = "test_id")
     private long testId;
     private String testName;
     private LocalDate testSampleDate;
     private BigDecimal testFee;
 
-    private LabTest(Builder builder) {
-        this.testId = builder.testId;
-        this.testName = builder.testName;
-        this.testSampleDate = builder.testSampleDate;
-        this.testFee = builder.testFee;
-    }
-    public long getTestId() {
-        return testId;
-    }
-
-    public String getTest() {
-        return testName;
-    }
-
-    public LocalDate getTestSampleDate() {
-        return testSampleDate;
-    }
-
-    public BigDecimal getTestFee() {
-        return testFee;
-    }
-
-    public void setTestId(long testId) {
-        this.testId = testId;
-    }
-
-    public void setTestName(String testName) {
-        this.testName = testName;
-    }
-
-    public void setTestSampleDate(LocalDate testSampleDate) {
-        this.testSampleDate = testSampleDate;
-    }
-
-    public void setTestFee(BigDecimal testFee) {
-        this.testFee = testFee;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        LabTest labTest = (LabTest) o;
+        return false;
     }
 
     @Override
-    public String toString() {
-        return "LabTest{" +
-                "testId=" + testId +
-                ", testName='" + testName + '\'' +
-                ", testSampleDate=" + testSampleDate +
-                ", testFee=" + testFee +
-                '}';
-    }
-    public static class Builder {
-        private long testId;
-        private String testName;
-        private LocalDate testSampleDate;
-        private BigDecimal testFee;
-
-        public Builder testId(long testId) {
-            this.testId = testId;
-            return this;
-        }
-        public Builder testName(String testName) {
-            this.testName = testName;
-            return this;
-        }
-        public Builder testSampleDate(LocalDate testSampleDate) {
-            this.testSampleDate = testSampleDate;
-            return this;
-        }
-        public Builder testFee(BigDecimal testFee) {
-            this.testFee = testFee;
-            return this;
-        }
-        public Builder copy(LabTest lbt) {
-            this.testId = lbt.testId;
-            this.testName = lbt.testName;
-            this.testSampleDate = lbt.testSampleDate;
-            this.testFee = lbt.testFee;
-            return this;
-        }
-        public LabTest build() {
-            return new LabTest(this);
-        }
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
+
